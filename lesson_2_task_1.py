d1becfbf8c42e5c434efb51cb586beaff71de7e1
Mrkorg1000 <korg1000@yandex.ru>
2021-04-11 19:54:10 +0300

    Homework for lesson 2

---
+++ lesson_2_task_1.py
@@ -0,0 +1,9 @@
+# Выяснить тип результата выражений:
+res1 = 15 * 3
+print(res1, type(res1))
+res2 = 15 / 3
+print(res2, type(res2))
+res3 = 15 // 2
+print(res3, type(res3))
+res4 = 15 ** 2
+print(res4, type(res4))
